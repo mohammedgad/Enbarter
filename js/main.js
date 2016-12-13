12fe30f10d21e5cb20e4e514df8c2edf16559cc5
Ahmed Kamel Taha <ahmedengu@gmail.com>
2016-12-13 16:57:58 +0200

    defer some js

--- js/main.js
+++ js/main.js
@@ -1217,3 +1217,43 @@ function angularCopy(source) {
         }
     }
 }
+
+
+function downloadJSAtOnload() {
+    var element = document.createElement("script");
+    element.src = "//mylivechat.com/chatinline.aspx?hccid=99228221";
+    document.body.appendChild(element);
+    element = document.createElement("script");
+    element.src = "//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-584fd4d3f9f8431f";
+    document.body.appendChild(element);
+
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+    ga('create', 'UA-88019035-1', 'auto');
+    ga('send', 'pageview');
+    (function (h, o, t, j, a, r) {
+        h.hj = h.hj || function () {
+                (h.hj.q = h.hj.q || []).push(arguments)
+            };
+        h._hjSettings = {hjid: 350598, hjsv: 5};
+        a = o.getElementsByTagName('head')[0];
+        r = o.createElement('script');
+        r.async = 1;
+        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window, document, '//static.hotjar.com/c/hotjar-', '.js?sv=');
+}
+if (window.addEventListener)
+    window.addEventListener("load", downloadJSAtOnload, false);
+else if (window.attachEvent)
+    window.attachEvent("onload", downloadJSAtOnload);
+else window.onload = downloadJSAtOnload;
