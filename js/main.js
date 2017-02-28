8c95f9e85b588e742981d37222994407878ad5f5
Ahmed Kamel Taha <ahmedengu@gmail.com>
2017-03-01 01:32:47 +0200

    rating

--- js/main.js
+++ js/main.js
@@ -147,6 +147,48 @@ app.directive('onFinishRender', function ($timeout) {
         }
     }
 });
+app.directive('starRating', function starRating() {
+    return {
+        restrict: 'EA',
+        template: '<ul class="star-rating" ng-class="{readonly: readonly}">' +
+        '  <li ng-repeat="star in stars" class="star" ng-class="{filled: star.filled}" ng-mouseover="toggle($index)">' +
+        '    <i class="glyphicon glyphicon-star"></i>' +
+        '  </li>' +
+        '</ul>',
+        scope: {
+            ratingValue: '=ngModel',
+            ratingStatic: '=ngRate',
+            max: '=?',
+            readonly: '=?'
+        },
+        link: function (scope, element, attributes) {
+            if (scope.max == undefined) {
+                scope.max = 5;
+            }
+            function updateStars(rate) {
+                scope.stars = [];
+                for (var i = 0; i < scope.max; i++) {
+                    scope.stars.push({
+                        filled: i < rate
+                    });
+                }
+            };
+            scope.toggle = function (index) {
+                if (scope.readonly == undefined || scope.readonly === false) {
+                    scope.ratingValue = index + 1;
+                    updateStars(scope.ratingValue);
+                }
+            };
+            if (scope.ratingValue == undefined) {
+                scope.ratingValue = 0;
+                updateStars(scope.ratingValue);
+            }
+            if (scope.ratingStatic != undefined)
+                updateStars(scope.ratingStatic);
+
+        }
+    };
+});
 app.controller('header', function ($scope, $location, $rootScope, $sce) {
     $rootScope.alertModal = function (message) {
         $scope.alertMessage = message;
